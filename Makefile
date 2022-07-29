985d0954ca33d7bc234bcbed90501b63b4db0062
tomershafir <tomer.shafir8@gmail.com>
2022-07-29 10:27:25 +0300

    feat: add randomized_select functions

--- Makefile
+++ Makefile
@@ -12,8 +12,10 @@ test: compile
 compile:
 	# checkout build in inference rules for C
 
-clean: clean_object_files
+clean: clean_object_files clean_executable
 	@echo "done"
 clean_object_files:
 	@echo "cleaning object files ..."
 	rm -f "$$(find "./" -type f -name "*.o")"
+clean_executable:
+	
--- src/select.c
+++ src/select.c
@@ -1,1 +1,44 @@
 #include "select.h"
+
+/* Partitions the accepted array by arr[end].
+   Note: the accepted array is mutated. */
+static int partition(int *arr, int start, int end);
+/* Partitions the accepted array by a randomly chosen array element.
+   Note: the accepted array is mutated. */
+static int randomized_partition(int *arr, int start, int end);
+
+/* Reports the accepted number of comparisions to stdout. */
+static void report_comp_metric(int num_of_comp);
+
+/* Sorts the accetped array in place using insertion sort algorithm.
+   Note: the accepted array is mutated. */
+/*static void insertion_sort(int *arr, int arr_length);*/
+
+/* Finds the ith smallest element in the accepted array and returns it.
+   Note: the accepted array is mutated. */
+int randomized_select(int *arr, int start, int end, int i) {
+    
+}
+
+/* Finds the ith smallest element in the accepted array and returns it.
+   Note: the accepted array is mutated. */
+int median_select(int *arr, int start, int end, int i) {
+
+}
+
+/* Partitions the accepted array by arr[end].
+   Note: the accepted array is mutated. */
+static int partition(int *arr, int start, int end) {
+
+}
+
+/* Partitions the accepted array by a randomly chosen array element.
+   Note: the accepted array is mutated. */
+static int randomized_partition(int *arr, int start, int end) {
+
+}
+
+/* Reports the accepted number of comparisions to stdout. */
+static void report_comp_metric(int num_of_comp) {
+
+}
