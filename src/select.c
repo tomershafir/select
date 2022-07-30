7469bb986bb438b54aa2eebdef94cb32cf107ea7
tomershafir <tomer.shafir8@gmail.com>
2022-07-30 14:37:48 +0300

    refactor: rename stuff

--- src/select.c
+++ src/select.c
@@ -12,15 +12,10 @@
   while (0)
 
 
-typedef struct index_and_comp_metric {
-  int index;
+typedef struct value_and_comp_metric {
+  int value;
   int comp_metric;
-} index_and_comp_metric;
-
-typedef struct median_and_comp_metric {
-  int median;
-  int comp_metric;
-} median_and_comp_metric;
+} value_and_comp_metric;
 
 /* Finds the ith smallest element in the accepted array and returns it and reports comparisions metric.
    Note: the accepted array is mutated. */
@@ -28,18 +23,18 @@ static int _randomized_select(int *arr, int start, int end, int i, int comp_metr
 
 /* Partitions the accepted array by a randomly chosen array element and returns it's new index and relevant comparisions metric.
    Note: the accepted array is mutated. */
-static index_and_comp_metric randomized_partition(int *arr, int start, int end);
+static value_and_comp_metric randomized_partition(int *arr, int start, int end);
 
 /* Partitions the accepted array by arr[end] and returns it's new index and relevant comparisions metric.
    Note: the accepted array is mutated. */
-static index_and_comp_metric partition(int *arr, int start, int end);
+static value_and_comp_metric partition(int *arr, int start, int end);
 
 /* Finds the ith smallest element in the accepted array and returns it and reports comparisions metric.
    Note: the accepted array is mutated. */
 int _median_select(int *arr, int arr_length, int i, int comp_metric, int seed);
 
 /* Finds the median of medians of the accepted array. */
-median_and_comp_metric find_median_of_medians(int *arr, int arr_length, int comp_metric, int seed);
+value_and_comp_metric find_median_of_medians(int *arr, int arr_length, int comp_metric, int seed);
 
 /* Sorts the accetped array in place using insertion sort algorithm and returns comparisions metric.
    Note: the accepted array is mutated. */
@@ -47,11 +42,11 @@ static int insertion_sort(int *arr, int arr_length);
 
 /* Partitions the accepted array by val and returns it's new index and relevant comparisions metric.
    Note: the accepted array is mutated. */
-static index_and_comp_metric partition_by(int *arr, int start, int end, int val);
+static value_and_comp_metric partition_by(int *arr, int start, int end, int val);
 
-static index_and_comp_metric index_of(int *arr, int start, int end, int val);
+static value_and_comp_metric index_of(int *arr, int start, int end, int val);
 
-static void report_comp_metric(int num_of_comp);
+static void report_comp_metric(int comp_metric);
 
 static void print_arr(int *arr, int arr_length);
 
@@ -68,7 +63,7 @@ int randomized_select(int *arr, int arr_length, int i) {
    Note: the accepted array is mutated. */
 static int _randomized_select(int *arr, int start, int end, int i, int comp_metric) {
   int left_subarr_including_pivot_length;
-  index_and_comp_metric wrapper;
+  value_and_comp_metric wrapper;
   
   if (start == end) {
     report_comp_metric(comp_metric);
@@ -77,23 +72,23 @@ static int _randomized_select(int *arr, int start, int end, int i, int comp_metr
 
   wrapper = randomized_partition(arr, start, end);
   comp_metric += wrapper.comp_metric;
-  left_subarr_including_pivot_length = wrapper.index - start + 1;
+  left_subarr_including_pivot_length = wrapper.value - start + 1;
   
   if (i == left_subarr_including_pivot_length) {
     report_comp_metric(comp_metric);
-    return arr[wrapper.index];
+    return arr[wrapper.value];
   }
 
   if (i < left_subarr_including_pivot_length) {
-    return _randomized_select(arr, start, wrapper.index - 1, i, comp_metric);
+    return _randomized_select(arr, start, wrapper.value - 1, i, comp_metric);
   } else {
-    return _randomized_select(arr, wrapper.index + 1, end, i - left_subarr_including_pivot_length, comp_metric);
+    return _randomized_select(arr, wrapper.value + 1, end, i - left_subarr_including_pivot_length, comp_metric);
   }
 }
 
 /* Partitions the accepted array by a randomly chosen array element and returns it's new index and relevant comparisions metric.
    Note: the accepted array is mutated. */
-static index_and_comp_metric randomized_partition(int *arr, int start, int end) {
+static value_and_comp_metric randomized_partition(int *arr, int start, int end) {
   int rndm, rndm_offset;
 
   srand(time(0));
@@ -107,15 +102,15 @@ static index_and_comp_metric randomized_partition(int *arr, int start, int end) 
 
 /* Partitions the accepted array by arr[end] and returns it's new index and relevant comparisions metric.
    Note: the accepted array is mutated. */
-static index_and_comp_metric partition(int *arr, int start, int end) {
+static value_and_comp_metric partition(int *arr, int start, int end) {
   int pivot, target, j;
-  int comp_counter = 0;
-  index_and_comp_metric wrapper;
+  int comp_metric = 0;
+  value_and_comp_metric wrapper;
 
   pivot = arr[end];
   target = start - 1;
   for (j = start; j < end; j++) {
-    comp_counter++;
+    comp_metric++;
     if (arr[j] <= pivot) {
       target++;
       SWAP_INT_BY_PTR(arr + target, arr + j);
@@ -123,8 +118,8 @@ static index_and_comp_metric partition(int *arr, int start, int end) {
   }
   SWAP_INT_BY_PTR(arr + target + 1, arr + end);
 
-  wrapper.index = target + 1;
-  wrapper.comp_metric = comp_counter;
+  wrapper.value = target + 1;
+  wrapper.comp_metric = comp_metric;
   return wrapper;
 }
 
@@ -147,21 +142,21 @@ int median_select_by_7(int *arr, int arr_length, int i) {
 /* Finds the ith smallest element in the accepted array and returns it and reports comparisions metric.
    Note: the accepted array is mutated. */
 int _median_select(int *arr, int arr_length, int i, int comp_metric, int seed) {
-  median_and_comp_metric median_wrapper;
-  index_and_comp_metric index_wrapper;
+  value_and_comp_metric median_wrapper;
+  value_and_comp_metric index_wrapper;
   int left_subarr_including_pivot_length;
 
   median_wrapper = find_median_of_medians(arr, arr_length, comp_metric, seed);
   comp_metric += median_wrapper.comp_metric;
   
-  index_wrapper = partition_by(arr, 0, arr_length - 1, median_wrapper.median);
+  index_wrapper = partition_by(arr, 0, arr_length - 1, median_wrapper.value);
   comp_metric += index_wrapper.comp_metric;
 
-  left_subarr_including_pivot_length = index_wrapper.index + 1;
+  left_subarr_including_pivot_length = index_wrapper.value + 1;
 
   if (i == left_subarr_including_pivot_length) {
     report_comp_metric(comp_metric);
-    return median_wrapper.median;
+    return median_wrapper.value;
   }
   
   if (i < left_subarr_including_pivot_length) {
@@ -172,17 +167,17 @@ int _median_select(int *arr, int arr_length, int i, int comp_metric, int seed) {
 }
 
 /* Finds the median of medians of the accepted array. */
-median_and_comp_metric find_median_of_medians(int *arr, int arr_length, int comp_metric, int seed) {
+value_and_comp_metric find_median_of_medians(int *arr, int arr_length, int comp_metric, int seed) {
   int num_of_medians, i, medians_index;
   int *medians;
-  median_and_comp_metric wrapper, tmp;
+  value_and_comp_metric wrapper, tmp;
   
   num_of_medians = (int) ceil(arr_length/seed);
   medians = (int *) malloc(num_of_medians * sizeof(int));
   medians_index = 0;
   
   if (arr_length <= seed) {
-    wrapper.median = arr[(int) floor(arr_length/2)];
+    wrapper.value = arr[(int) floor(arr_length/2)];
     wrapper.comp_metric = comp_metric;
     return wrapper;
   } 
@@ -202,51 +197,51 @@ median_and_comp_metric find_median_of_medians(int *arr, int arr_length, int comp
    Note: the accepted array is mutated. */
 static int insertion_sort(int *arr, int arr_length) {
   int i, j, key;
-  int comp_counter;
+  int comp_metric;
 
-  comp_counter = 0;
+  comp_metric = 0;
   for (i = 1; i < arr_length; i++) {
     key = arr[i];
     j = i-1;
     while (j > -1 && key < arr[j]) {
-      comp_counter++;
+      comp_metric++;
       arr[j+1] = arr[j];
       j--;
     }
     arr[j+1] = key;
   }
-  return comp_counter;
+  return comp_metric;
 }
 
 /* Partitions the accepted array by val and returns it's new index and relevant comparisions metric.
    Note: the accepted array is mutated. */
-static index_and_comp_metric partition_by(int *arr, int start, int end, int val) {
-  index_and_comp_metric wrapper, tmp;
+static value_and_comp_metric partition_by(int *arr, int start, int end, int val) {
+  value_and_comp_metric wrapper, tmp;
 
   tmp = index_of(arr, start, end, val);
-  SWAP_INT_BY_PTR(arr + tmp.index, arr + end);
+  SWAP_INT_BY_PTR(arr + tmp.value, arr + end);
 
   wrapper = partition(arr, start, end);
   wrapper.comp_metric += tmp.comp_metric;
   return wrapper;
 }
 
-static index_and_comp_metric index_of(int *arr, int start, int end, int val) {
+static value_and_comp_metric index_of(int *arr, int start, int end, int val) {
   int j;
-  int comp_counter = 0;
-  index_and_comp_metric wrapper;
+  int comp_metric = 0;
+  value_and_comp_metric wrapper;
 
   for (j = start; j <= end; j++) {
-    comp_counter++;
+    comp_metric++;
     if(arr[j] == val) {
-      wrapper.index = j;
-      wrapper.comp_metric = comp_counter;
+      wrapper.value = j;
+      wrapper.comp_metric = comp_metric;
       return wrapper;
     }
   }
 
-  wrapper.index = -1;
-  wrapper.comp_metric = comp_counter;
+  wrapper.value = -1;
+  wrapper.comp_metric = comp_metric;
   return wrapper;
 }
 
@@ -259,6 +254,6 @@ static void print_arr(int *arr, int arr_length) {
   printf("\n");
 }
 
-static void report_comp_metric(int num_of_comp) {
-  printf("number of comparisons: %d\n", num_of_comp);
+static void report_comp_metric(int comp_metric) {
+  printf("number of comparisons: %d\n", comp_metric);
 }
